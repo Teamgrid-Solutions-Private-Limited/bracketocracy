6d42a320e1fe20d0bc8602de655e226673005f1a
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2024-08-02 16:33:47 +0530

    countdown controller added

---
+++ server/routes/countdownRoutes.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const { updateCount, countDelete, searchById, viewAll, addCountdown } = require('../controllers/CountdownController');
+ 
+
+
+const router = express.Router();
+router.post('/create',addCountdown);
+router.get('/show',viewAll);
+router.get('/search/:id',searchById);
+router.delete('/delete/:id',countDelete);
+router.put('/update/:id',updateCount);
+
+
+module.exports=router;
