c5bb80521e1739b6cd8fd49621471a7e0cc7b168
sarukteamgrid <sksaruk.ali@teamgrid.io>
2024-08-19 11:44:55 +0530

    sponsercontroller image path

--- server/controllers/sponsorController.js
+++ server/controllers/sponsorController.js
@@ -2,10 +2,15 @@ const Sponsor = require("../model/sponsorSchema");
 const upload = require("../middleware/fileUpload");
 
 const BASE_URL = "http://localhost:6010/";
-const upload_URL = BASE_URL + "my-uploads/";
+const upload_URL = BASE_URL + "images/";
 
 class SponsorController {
-  static handleError = (res, error, message = "Internal Server Error", status = 500) => {
+  static handleError = (
+    res,
+    error,
+    message = "Internal Server Error",
+    status = 500
+  ) => {
     console.error(message, error);
     res.status(status).json({ error: message });
   };
@@ -13,7 +18,12 @@ class SponsorController {
   static handleFileUpload = (req, res, next) => {
     upload.single("logo")(req, res, (err) => {
       if (err) {
-        return SponsorController.handleError(res, err, "Error uploading file", 400);
+        return SponsorController.handleError(
+          res,
+          err,
+          "Error uploading file",
+          400
+        );
       }
       next();
     });
@@ -36,7 +46,9 @@ class SponsorController {
           });
 
           const result = await sponsor.save();
-          res.status(201).json({ message: "Sponsor added successfully", data: result });
+          res
+            .status(201)
+            .json({ message: "Sponsor added successfully", data: result });
         } catch (err) {
           SponsorController.handleError(res, err, "Error saving sponsor");
         }
@@ -55,7 +67,9 @@ class SponsorController {
         return res.status(404).json({ error: "Sponsor not found" });
       }
 
-      res.status(200).json({ message: "Sponsor deleted successfully", data: result });
+      res
+        .status(200)
+        .json({ message: "Sponsor deleted successfully", data: result });
     } catch (err) {
       SponsorController.handleError(res, err, "Error deleting sponsor");
     }
@@ -64,7 +78,9 @@ class SponsorController {
   static displayAll = async (req, res) => {
     try {
       const data = await Sponsor.find();
-      res.status(200).json({ message: "Sponsor list retrieved successfully", data });
+      res
+        .status(200)
+        .json({ message: "Sponsor list retrieved successfully", data });
     } catch (err) {
       SponsorController.handleError(res, err, "Error retrieving sponsors");
     }
@@ -108,7 +124,12 @@ class SponsorController {
         }
 
         const updatedSponsor = await sponsor.save();
-        res.status(200).json({ message: "Sponsor updated successfully", data: updatedSponsor });
+        res
+          .status(200)
+          .json({
+            message: "Sponsor updated successfully",
+            data: updatedSponsor,
+          });
       });
     } catch (err) {
       SponsorController.handleError(res, err, "Error updating sponsor");
