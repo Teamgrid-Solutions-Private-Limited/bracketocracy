b7237be879f8af1d1522af7435bfed89d10c5bb8
sarukteamgrid <sksaruk.ali@teamgrid.io>
2024-08-22 15:00:35 +0530

    branch some changes in usercontroller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,9 +7,9 @@ const {
   getUserById,
   deleteUser,
   updateUser,
+  resetPassword,
 } = require("../controllers/userController");
 const checkUser = require("../middleware/checkUser");
-const checkAdmin = require("../middleware/checkAdmin");
 
 Router.post("/user/create", addUser);
 Router.post("/user/login", login);
@@ -17,6 +17,7 @@ Router.post("/admin/login", login);
 Router.get("/user/view", checkUser, getUser);
 Router.get("/user/viewbyid/:id", getUserById);
 Router.put("/user/update/:id", updateUser);
+Router.put("/user/reset/:id", resetPassword);
 Router.delete("/user/delete/:id", deleteUser);
 
 module.exports = Router;
