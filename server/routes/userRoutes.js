bd26fc306c09f01b3c5b8f7e6148a4712cf9b627
mdtanvirteam <mdtanvir.ahmed@teamgrid.io>
2024-09-02 11:56:52 +0530

    routes updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -15,7 +15,7 @@ const checkUser = require("../middleware/checkUser");
 Router.post("/user/create", addUser);
 Router.post("/user/login", login);
 Router.post("/admin/login", login);
-Router.get("/user/view", checkUser, getUser);
+Router.get("/user/view",getUser);
 Router.get("/user/viewbyid/:id", getUserById);
 Router.put("/user/update/:id", updateUser);
 Router.put("/user/updateuser/:id", updateAdminUser);
